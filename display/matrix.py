ea3a5c920fc60176651e7f8c802bffb47d3d2b2c
Dave MacGugan <dmagoo@rocketmail.com>
2017-11-20 05:53:09 +0000

    added stubs for surfaces, masks and rectangles. added a routine to write surface/mask to strip

--- display/matrix.py
+++ display/matrix.py
@@ -2,17 +2,23 @@ import time
 from itertools import cycle
 from neopixel import *
 from spritesheet import rip_ascii_sprites
+from mask import Mask
+from surface import Surface
+from rectangle import Rectangle
 
 COLOR_BLACK = Color(0, 0, 0)
 COLOR_RED = Color(255, 0, 0)
 COLOR_GREEN = Color(0, 255, 0)
 COLOR_BLUE = Color(0, 0, 255)
+COLOR_YELLOW = Color(255,255,0)
 COLOR_WHITE = Color(255, 255, 255)
 
 class Matrix:
 
     _matrix_map = []
     _ascii = []
+    _master_surface = None
+    _master_mask = None
 
     def __init__(self, width, height):
 
@@ -31,6 +37,7 @@ class Matrix:
 
         self.__createMatrixMap()
         self.__createAlphabet()
+        self.__createSurfaces()
         
     def clearStrip(self):
         return self.setStripColor(COLOR_BLACK)
@@ -55,11 +62,6 @@ class Matrix:
         for y in range(8):
             x = 0
             for i in range(len(my_str)):
-                #print i
-                #print my_str
-                #print my_str[i]
-                #print ord(my_str[i])
-                #print self._ascii
                 img = self._ascii.get(ord(my_str[i]))
                 for pixel in img[y]:
                     self.strip.setPixelColor(self._matrix_map[y][x], COLOR_BLUE if pixel else COLOR_BLACK)
@@ -70,8 +72,11 @@ class Matrix:
         self.strip.begin()
         self.clearStrip().strip.show()
         time.sleep(1)
-        self.clearStrip().drawLetters().strip.show()
-        time.sleep(5)
+        self.write().strip.show()
+        print "write sleep 10"
+        time.sleep(10)
+        self.drawLetters().strip.show()
+        time.sleep(1)
         self.setStripColor(COLOR_RED).strip.show()
         time.sleep(1)
         self.setStripColor(COLOR_GREEN).strip.show()
@@ -90,6 +95,19 @@ class Matrix:
             time.sleep(100/1000.0)
         self.clearStrip().strip.show()
 
+    def write(self):
+        """dump the master matrix to the strip, using mask"""
+        for y in range(self.height):
+            for x in range(self.width):
+                self.strip.setPixelColor(
+                    self._matrix_map[y][x],
+                    int(
+                        self._master_surface.pixels[y][x] *
+                        self._master_mask.pixels[y][x]
+                    )
+                )
+
+        return self
     def __createMatrixMap(self):
         """
         form a mapping in a serpentine pattern zig-zagging top to bottom
@@ -107,5 +125,26 @@ class Matrix:
         print "done"
 
     def __createAlphabet(self):
+        print "ripping alphabet sprites"
         self._ascii = rip_ascii_sprites("/home/pi/RetroPie-Custom/assets/glyphs/C64-font.bmp")
-        print "alphabet done"
+
+
+    def __createSurfaces(self):
+        viewport = Rectangle((0,0,self.width,self.height))
+        self._master_surface = Surface(viewport.size, viewport)
+        self._master_surface.pixels.fill(COLOR_YELLOW)
+        self._master_mask = Mask(viewport.size, viewport)
+        self._master_mask.pixels[0][0] = 0
+        self._master_mask.pixels[1][0] = 0
+        self._master_mask.pixels[2][0] = 0
+        self._master_mask.pixels[3][0] = 0
+        self._master_mask.pixels[4][0] = 0
+        self._master_mask.pixels[5][0] = 0
+        self._master_mask.pixels[6][0] = 0
+        self._master_mask.pixels[0][22] = 0
+        self._master_mask.pixels[1][23] = 0
+        self._master_mask.pixels[2][24] = 0
+        self._master_mask.pixels[3][25] = 0
+        self._master_mask.pixels[4][26] = 0
+        self._master_mask.pixels[5][27] = 0
+        self._master_mask.pixels[6][28] = 0        
